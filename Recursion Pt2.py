2030a77a09ae2ed7c356915ab134c10fc141ea18
Diego E <diegoescobarmanrique2@gmail.com>
2026-09-02 15:22:35 -0600

    Ejercicio 2 calcular capital

---
+++ Recursion Pt2.py
@@ -0,0 +1,13 @@
+def calcular_capital(m, x, n):
+    if n == 0:
+        return m
+    else:
+        return calcular_capital(m, x, n - 1) * (1 + x / 100)
+
+
+capital_inicial = float(input("Ingresa el capital inicial (m): "))
+tasa_interes = float(input("Ingresa la tasa de interes anual (X%): "))
+anios = int(input("Ingresa el numero de años: "))
+
+capital_final = calcular_capital(capital_inicial, tasa_interes, anios)
+print("El capital al cabo de", anios, "anios sera:", capital_final)
